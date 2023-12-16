9438f9fbcf5b6695f1d6a849fb588b9ead368c9e
anon <anon@anon.anon>
2023-12-16 14:50:44 +0100

    genrate tags file

--- source/ctags.c
+++ source/ctags.c
@@ -1,3 +1,15 @@
+#include "global.h"
+
+#include <unistd.h>
+
 int gen_tags_file(void) {
+	const char * execve_args[nsrcfiles + 1];
+	execve_args[0] = "ctags";
+	for (int i = 0; i < nsrcfiles; i++) {
+		execve_args[i + 1] = srcfiles[i];
+	}
+
+	execvp(execve_args[0], execve_args);
+
 	return 0;
 }
