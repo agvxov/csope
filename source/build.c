6b5c851e0dd6c9225e1133e9aafe2cbbeb280393
anon <anon@anon.anon>
2024-11-15 20:01:39 +0100

    remove legacy macro

--- source/build.c
+++ source/build.c
@@ -46,11 +46,7 @@
 #include "version.inc"
 #include "vp.h"
 
-#if defined(USE_NCURSES) && !defined(RENAMED_NCURSES)
 # include <ncurses.h>
-#else
-# include <curses.h>
-#endif
 
 /* Exported variables: */
 
