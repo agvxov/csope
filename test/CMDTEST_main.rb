94f8420d0a34a3271deaa5b546128155e861d8f6
anon <anon@anon.anon>
2026-01-15 20:17:55 +0100

    revisit tests; all passing

--- test/CMDTEST_main.rb
+++ test/CMDTEST_main.rb
@@ -1,12 +1,11 @@
-# NOTE: Kernel mode should be used accross the board,
-#        because system headers are volatile.
-#       Writting tests for them would be foolish.
-#       They could even introduce interfering
-#        symbols in the future by pure chance,
-#        if not completely ignored.
+# NOTE:
+#   * Kernel mode should be used accross the board,
+#      because system headers are volatile.
+#     Writting tests for them would be foolish.
+#     They could even introduce interference
+#      symbols in the future by pure chance,
+#      if not completely ignored.
 
-# The following variables are magick numbers based on `dummy_project/`.
-$f_definition_line = 5
 
 class CMDTEST_misc_batch < Cmdtest::Testcase
   def test_no_arg
@@ -19,21 +18,35 @@ class CMDTEST_misc_batch < Cmdtest::Testcase
 end
 
 class CMDTEST_dummy_project < Cmdtest::Testcase
+  # The following variables are magick numbers based on `dummy_project/`.
+  @@f_definition_line = 5
+  @@f_mention_count   = 2
+
   def setup
     import_directory "test/dummy_project/", "./dummy_project/"
   end
 
-  def test_find_f
-    cmd "csope -k -L -0 f -s dummy_project/" do
+  def test_do_not_explode
+    cmd "csope -k -b -s dummy_project" do
       created_files ["cscope.out"]
-      stdout_equal /\A(.*\n){2}\Z/
+      stderr_equal /.+/
+      exit_status 0
     end
   end
 
   def test_find_def_f
     cmd "csope -k -L -1 f -s dummy_project/" do
       created_files ["cscope.out"]
-      stdout_equal /\A.+#{$f_definition_line}.+\n\Z/
+      stderr_equal /.+/
+      stdout_equal /\A.+#{@@f_definition_line}.+\n\Z/
+    end
+  end
+
+  def test_find_f
+    cmd "csope -k -L -0 f -s dummy_project/" do
+      created_files ["cscope.out"]
+      stderr_equal /.+/
+      stdout_equal /\A(.*\n){#{@@f_mention_count}}\Z/
     end
   end
 end
